66ed4420aba6e544903817c1a7a161eae5692f6b
sarkiisov <sarkisov1337@gmail.com>
2025-03-12 00:19:57 +0300

    chore: remove unused layout

--- src/components/layouts/Center/Center.tsx
+++
@@ -1,7 +0,0 @@
-import { HTMLAttributes } from 'react'
-import './Center.styles.css'
-import clsx from 'clsx'
-
-export const Center = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
-  return <div className={clsx('center', className)} {...props} />
-}
