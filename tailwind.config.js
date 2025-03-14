41c7b5407b7ae599f783d38cb7dc0340d3ec599c
sarkiisov <sarkisov1337@gmail.com>
2025-03-15 00:19:54 +0300

    feat: tailwind forms & config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import forms from '@tailwindcss/forms'
+
+import colors from 'tailwindcss/colors'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {}
+  },
+  plugins: [forms]
+}
